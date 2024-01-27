a84789d48da9028e36ca36c8f292f52e13134b75
Aliyu Abdullahi <aliyuabdullahi074@gmail.com>
2024-01-27 20:42:27 +0100

    Update

--- AutoInvest.Infrastructure/ExternalAPI/PaystackHelper.cs
+++ AutoInvest.Infrastructure/ExternalAPI/PaystackHelper.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AutoInvest.Infrastructure.ExternalAPI
 {
-    internal class PaystackHelper
+    public class PaystackHelper
     {
+        public HttpClient PaystackClient { get; set; }
+       /* public PaystackHelper(IConfiguration configuration)
+        {
+            string paystackSecret = configuration.GetSection("PayStackKey");
+            PaystackClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.paystack.co/")
+            };
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", paystackSecret);
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }*/
     }
 }
